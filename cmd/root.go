baa011c7c09ffd9a307dcdaded555af7c269d604
reubenmiller <reuben.d.miller@gmail.com>
2024-11-07 22:03:36 +0100

    update main command description

--- cmd/root.go
+++ cmd/root.go
@@ -26,14 +26,8 @@ var buildBranch string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "tedge-container",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:     "tedge-container",
+	Short:   "thin-edge.io container engine plugin to manage and monitor containers on a device",
 	Version: fmt.Sprintf("%s (branch=%s)", buildVersion, buildBranch),
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		return SetLogLevel()
